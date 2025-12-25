e8f35687db515445d2d0a38ebb074edc493443eb
Jacob Nguyen <jacobptnguyen@gmail.com>
2025-12-24 17:23:18 -0800

    update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects: React.FC = () => {
     <section id="projects" className="pt-0 pb-8 px-6 bg-black text-white">
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project) => (
+          {[...projects].reverse().map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </div>
